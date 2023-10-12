8e3b37dbfd018c9ac27f2af96e4276de9851fe75
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-10-12 15:59:53 +0300

    1.8.0-4

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.8.0-3"
+let bnbPackageVersion: Version = "1.8.0-4"
 
 let package = Package(
     name: "BNBPoseEstimation",
@@ -11,19 +11,12 @@ let package = Package(
     products: [
         .library(
             name: "BNBPoseEstimation",
-            targets: ["BNBPoseEstimation"]
-        ),
-        .library(
-            name: "BNBSdkCore",
-            targets: ["BNBSdkCore"]
-        ),
-        .library(
-            name: "BNBEffectPlayer",
-            targets: ["BNBEffectPlayer"]
-        ),
-        .library(
-            name: "BNBScripting",
-            targets: ["BNBScripting"]
+            targets: [
+                "BNBPoseEstimation",
+                "BNBSdkCore",
+                "BNBEffectPlayer",
+                "BNBScripting"
+            ]
         ),
     ],
     dependencies: [
